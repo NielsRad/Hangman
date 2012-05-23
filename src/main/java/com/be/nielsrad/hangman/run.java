f373633c9c87761afac3c8da9a54ba9f0c11ed9d
Niels Rademaekers <niels.rademaekers@miiatech.com>
2012-05-23 15:54:24 -0700

    Added the main class code

--- src/main/java/com/be/nielsrad/hangman/run.java
+++ src/main/java/com/be/nielsrad/hangman/run.java
@@ -4,10 +4,32 @@
  */
 package com.be.nielsrad.hangman;
 
+import com.be.nielsrad.hangman.controller.Galgje;
+import com.be.nielsrad.hangman.view.MainView;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author NielsR
  */
 public class run {
-    
+    public static void main(String[] args) {
+        //TODO: Java 7 heeft standaard Nimbus Look and Feel
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+
+                    UIManager.setLookAndFeel(info.getClassName());
+
+                    break;
+                }
+            }
+         //TODO: In Java 7 kan je meerdere fouten opvangen!
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+
+        }
+        new MainView(new Galgje()).setVisible(true);
+    }
 }
+
